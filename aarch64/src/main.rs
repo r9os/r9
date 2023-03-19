b2a9cb984293dd10fe28a01023cc6f5532061b66
Graham MacDonald <grahamamacdonald@gmail.com>
2023-03-19 16:22:02 +0000

    Enable aarch64 mmu immediately Before entering rust, we set up a single 2GiB page, enable the MMU and jump to the higher half. Also adds some registers and tests to help decipher any issues.

--- aarch64/src/main.rs
+++ aarch64/src/main.rs
@@ -10,11 +10,58 @@ mod devcons;
 mod registers;
 mod trap;
 
+use core::ffi::c_void;
+use core::ptr;
 use port::fdt::DeviceTree;
 use port::println;
 
 #[cfg(not(test))]
 core::arch::global_asm!(include_str!("l.S"));
+
+fn print_binary_sections() {
+    extern "C" {
+        static boottext: *const c_void;
+        static eboottext: *const c_void;
+        static text: *const c_void;
+        static etext: *const c_void;
+        static rodata: *const c_void;
+        static erodata: *const c_void;
+        static data: *const c_void;
+        static edata: *const c_void;
+        static bss: *const c_void;
+        static end: *const c_void;
+    }
+
+    let bootcode_start: u64 = unsafe { ptr::addr_of!(boottext) as u64 };
+    let bootcode_end: u64 = unsafe { ptr::addr_of!(eboottext) as u64 };
+    let bootcode_size: u64 = bootcode_end - bootcode_start;
+
+    let text_start: u64 = unsafe { ptr::addr_of!(text) as u64 };
+    let text_end: u64 = unsafe { ptr::addr_of!(etext) as u64 };
+    let text_size: u64 = text_end - text_start;
+
+    let rodata_start: u64 = unsafe { ptr::addr_of!(rodata) as u64 };
+    let rodata_end: u64 = unsafe { ptr::addr_of!(erodata) as u64 };
+    let rodata_size: u64 = rodata_end - rodata_start;
+
+    let data_start: u64 = unsafe { ptr::addr_of!(data) as u64 };
+    let data_end: u64 = unsafe { ptr::addr_of!(edata) as u64 };
+    let data_size: u64 = data_end - data_start;
+
+    let bss_start: u64 = unsafe { ptr::addr_of!(bss) as u64 };
+    let bss_end: u64 = unsafe { ptr::addr_of!(end) as u64 };
+    let bss_size: u64 = bss_end - bss_start;
+
+    let total_size: u64 = bss_end - bootcode_start;
+
+    println!("Binary sections:");
+    println!("  boottext:\t{:#x}-{:#x} ({:#x})", bootcode_start, bootcode_end, bootcode_size);
+    println!("  text:\t\t{:#x}-{:#x} ({:#x})", text_start, text_end, text_size);
+    println!("  rodata:\t{:#x}-{:#x} ({:#x})", rodata_start, rodata_end, rodata_size);
+    println!("  data:\t\t{:#x}-{:#x} ({:#x})", data_start, data_end, data_size);
+    println!("  bss:\t\t{:#x}-{:#x} ({:#x})", bss_start, bss_end, bss_size);
+    println!("  total:\t{:#x}-{:#x} ({:#x})", bootcode_start, bss_end, total_size);
+}
 
 #[no_mangle]
 pub extern "C" fn main9(dtb_ptr: u64) {
@@ -26,6 +73,7 @@ pub extern "C" fn main9(dtb_ptr: u64) {
     println!();
     println!("r9 from the Internet");
     println!("DTB found at: {:#x}", dtb_ptr);
+    print_binary_sections();
 
     // Assume we've got MMU set up, so drop early console for the locking console
     port::devcons::drop_early_console();
--- aarch64/src/registers.rs
+++ aarch64/src/registers.rs
@@ -5,10 +5,10 @@ use num_enum::TryFromPrimitive;
 bitstruct! {
     #[derive(Copy, Clone)]
     pub struct EsrEl1(pub u64) {
-        iss: u32 = 0 .. 25;
+        iss: u32 = 0..25;
         il: bool = 25;
         ec: u8 = 26..32;
-        iss2: u8 = 32 .. 37;
+        iss2: u8 = 32..37;
     }
 }
 
@@ -63,4 +63,149 @@ pub enum ExceptionClass {
     WatchpointLowerEl = 52,
     WatchpointSameEl = 53,
     Brk = 60,
+}
+
+bitstruct! {
+    #[derive(Copy, Clone)]
+    pub struct EsrEl1IssInstructionAbort(pub u32) {
+        ifsc: u8 = 0..6;
+        s1ptw: bool = 7;
+        ea: bool = 9;
+        fnv: bool = 10;
+        set: u8 = 11..13;
+    }
+}
+
+impl EsrEl1IssInstructionAbort {
+    pub fn from_esr_el1(r: EsrEl1) -> Option<EsrEl1IssInstructionAbort> {
+        r.exception_class()
+            .ok()
+            .filter(|ec| *ec == ExceptionClass::InstructionAbortSameEl)
+            .map(|_| EsrEl1IssInstructionAbort(r.iss()))
+    }
+
+    pub fn instruction_fault(&self) -> Result<InstructionFaultStatusCode, u8> {
+        InstructionFaultStatusCode::try_from(self.ifsc()).map_err(|e| e.number)
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
+#[repr(u8)]
+pub enum InstructionFaultStatusCode {
+    AddressSizeFaultLevel0 = 0,
+    AddressSizeFaultLevel1 = 1,
+    AddressSizeFaultLevel2 = 2,
+    AddressSizeFaultLevel3 = 3,
+    TranslationFaultLevel0 = 4,
+    TranslationFaultLevel1 = 5,
+    TranslationFaultLevel2 = 6,
+    TranslationFaultLevel3 = 7,
+    AccessFlagFaultLevel0 = 8,
+    AccessFlagFaultLevel1 = 9,
+    AccessFlagFaultLevel2 = 10,
+    AccessFlagFaultLevel3 = 11,
+    PermissionFaultLevel0 = 12,
+    PermissionFaultLevel1 = 13,
+    PermissionFaultLevel2 = 14,
+    PermissionFaultLevel3 = 15,
+    SyncExtAbortNotOnWalkOrUpdate = 16,
+    SyncExtAbortOnWalkOrUpdateLevelNeg1 = 19,
+    SyncExtAbortOnWalkOrUpdateLevel0 = 20,
+    SyncExtAbortOnWalkOrUpdateLevel1 = 21,
+    SyncExtAbortOnWalkOrUpdateLevel2 = 22,
+    SyncExtAbortOnWalkOrUpdateLevel3 = 23,
+    SyncParityOrEccErrOnMemAccessNotOnWalk = 24,
+    SyncParityOrEccErrOnMemAccessOnWalkOrUpdateLevelNeg1 = 27,
+    SyncParityOrEccErrOnMemAccessOnWalkOrUpdateLevel0 = 28,
+    SyncParityOrEccErrOnMemAccessOnWalkOrUpdateLevel1 = 29,
+    SyncParityOrEccErrOnMemAccessOnWalkOrUpdateLevel2 = 30,
+    SyncParityOrEccErrOnMemAccessOnWalkOrUpdateLevel3 = 31,
+    GranuleProtectFaultOnWalkOrUpdateLevelNeg1 = 35,
+    GranuleProtectFaultOnWalkOrUpdateLevel0 = 36,
+    GranuleProtectFaultOnWalkOrUpdateLevel1 = 37,
+    GranuleProtectFaultOnWalkOrUpdateLevel2 = 38,
+    GranuleProtectFaultOnWalkOrUpdateLevel3 = 39,
+    GranuleProtectFaultNotOnWalkOrUpdateLevel = 40,
+    AddressSizeFaultLevelNeg1 = 41,
+    TranslationFaultLevelNeg1 = 43,
+    TlbConflictAbort = 48,
+    UnsupportedAtomicHardwareUpdateFault = 49,
+}
+
+bitstruct! {
+    #[derive(Copy, Clone)]
+    pub struct Vaddr4K4K(pub u64) {
+        offset: u16 = 0..12;
+        l4idx: u16 = 12..21;
+        l3idx: u16 = 21..30;
+        l2idx: u16 = 30..39;
+        l1idx: u16 = 39..48;
+    }
+}
+
+bitstruct! {
+    #[derive(Copy, Clone)]
+    pub struct Vaddr4K1G(pub u64) {
+        offset: u32 = 0..30;
+        l2idx: u16 = 30..39;
+        l1idx: u16 = 39..48;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // This test is useful for making sense of early-stage exceptions.  Qemu
+    // will report an exception of the form below.  Copy the ESR value into
+    // this test to break it down.
+    //
+    // Exception return from AArch64 EL2 to AArch64 EL1 PC 0x8006c
+    // Taking exception 3 [Prefetch Abort] on CPU 0
+    // ...from EL1 to EL1
+    // ...with ESR 0x21/0x86000004
+    // ...with FAR 0x80090
+    // ...with ELR 0x80090
+    // ...to EL1 PC 0x200 PSTATE 0x3c5
+    #[test]
+    fn test_parse_esr_el1() {
+        let r = EsrEl1(0x86000004);
+        assert_eq!(r.exception_class().unwrap(), ExceptionClass::InstructionAbortSameEl);
+        assert_eq!(
+            EsrEl1IssInstructionAbort::from_esr_el1(r).unwrap().instruction_fault().unwrap(),
+            InstructionFaultStatusCode::TranslationFaultLevel0
+        );
+    }
+
+    #[test]
+    fn breakdown_vadder() {
+        let va = Vaddr4K4K(0xffff_8000_0000_0000);
+        assert_eq!(va.l1idx(), 256);
+        assert_eq!(va.l2idx(), 0);
+        assert_eq!(va.l3idx(), 0);
+        assert_eq!(va.l4idx(), 0);
+        assert_eq!(va.offset(), 0);
+
+        let va = Vaddr4K4K(0x0000_0000_0008_00a8);
+        assert_eq!(va.l1idx(), 0);
+        assert_eq!(va.l2idx(), 0);
+        assert_eq!(va.l3idx(), 0);
+        assert_eq!(va.l4idx(), 128);
+        assert_eq!(va.offset(), 168);
+
+        let va = Vaddr4K1G(0xffff_8000_0000_0000);
+        assert_eq!(va.l1idx(), 256);
+        assert_eq!(va.l2idx(), 0);
+        assert_eq!(va.offset(), 0);
+
+        let va = Vaddr4K1G(0x0000_0000_0008_00a8);
+        assert_eq!(va.l1idx(), 0);
+        assert_eq!(va.l2idx(), 0);
+        assert_eq!(va.offset(), 524456);
+
+        let va = Vaddr4K1G(0xffff_8000_0010_00c8);
+        assert_eq!(va.l1idx(), 256);
+        assert_eq!(va.l2idx(), 0);
+        assert_eq!(va.offset(), 0x1000c8);
+    }
 }
