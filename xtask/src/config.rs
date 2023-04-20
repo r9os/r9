95443f73ea958d989380907925056d56bdb5afe9
Stefan Hertenberger <erde74@gmail.com>
2023-04-20 10:08:52 +0200

    cleanup and documentation
    Signed-off-by: Stefan Hertenberger <erde74@gmail.com>

--- xtask/src/config.rs
+++ xtask/src/config.rs
@@ -1,3 +1,5 @@
+/// Test
+///
 use crate::{cargo, Command, Profile};
 
 use serde::{Deserialize, Serialize};
@@ -8,18 +10,44 @@ use std::{
     process::exit,
 };
 
+/// build section
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Build {
+    /// The buildflags controls build-time operations and compiler settings.
     pub buildflags: Option<Vec<String>>,
+
+    /// A list of custom flags to pass to all compiler invocations that Cargo performs.
     pub rustflags: Option<Vec<String>>,
+
+    /// Build for the given architecture.
     pub target: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Link {
-    pub conf: HashMap<String, String>,
-}
+/// Config section
+/// currently available configuration sections are dev, ip, link, nodev, nouart
+/// the section name is becomes the prefix for the configuration option
+/// example usage for section "dev"
+/// ```toml
+///  dev = [
+///     'arch',
+///     'cap',
+///     'foo="baz"'
+///  ]
+/// ```
+///  this will create the following configuration options
+///  dev_arch, dev_cap and dev_foo="baz"
+///
+/// usage example:
+///  ```rust
+/// #[cfg(dev_arch)]
+/// pub mod devarch;
+/// ```
+/// ```rust
+/// #[cfg(dev_foo = "baz")]
+/// pub mod foobaz;
+/// ```
 
+/// config section
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub dev: Option<Vec<String>>,
@@ -28,9 +56,18 @@ pub struct Config {
     pub link: Option<Vec<String>>,
     pub nodev: Option<Vec<String>>,
     pub nouart: Option<Vec<String>>,
-    pub platform: String,
+
+    /// platform/board possible values: empty, raspi3b, vfive2, nezha, virt etc.
+    ///
+    /// example usage
+    /// ´´´rust
+    /// #[cfg(platform = "virt")]
+    /// pub mod virt;
+    /// ```
+    pub platform: Option<String>,
 }
 
+/// the TOML document
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Configuration {
     pub build: Option<Build>,
@@ -49,7 +86,7 @@ pub fn read_config(filename: String) -> Configuration {
     let config: Configuration = match toml::from_str(&contents) {
         Ok(d) => d,
         Err(e) => {
-            eprintln!("ConfigFile: Unable to load data from `{}`", filename);
+            eprintln!("TOML: Unable to load data from `{}`", filename);
             eprintln!("{e}");
             exit(1);
         }
@@ -57,6 +94,7 @@ pub fn read_config(filename: String) -> Configuration {
     config
 }
 
+/// task could be 'build', 'clippy'
 pub fn generate_args(
     task: &str,
     config: &Configuration,
@@ -64,6 +102,7 @@ pub fn generate_args(
     profile: &Profile,
     wks_path: &str,
 ) -> Command {
+    // we have to pass the rustflags all at once, so collect them
     let mut rustflags: Vec<String> = Vec::new();
     let mut cmd = Command::new(cargo());
     cmd.arg(task);
@@ -74,12 +113,14 @@ pub fn generate_args(
             cmd.arg("--target").arg(target);
 
             if let Some(flags) = &config.buildflags {
+                // add the buildflags to the command
                 for f in flags {
                     cmd.arg(f);
                 }
             }
 
             if let Some(flags) = &config.rustflags {
+                // store the passed rustflags temporarily
                 for f in flags {
                     rustflags.push(f.to_string());
                 }
@@ -88,9 +129,7 @@ pub fn generate_args(
     }
 
     if let Some(config) = &config.config {
-        rustflags.push("--cfg".into());
-        rustflags.push(format!("platform=\"{}\"", config.platform));
-
+        // if the target will use features make them available
         if let Some(features) = &config.features {
             let mut joined = features.join(",");
             if !features.is_empty() && joined.is_empty() {
@@ -99,45 +138,67 @@ pub fn generate_args(
             cmd.arg(format!("--features={joined}"));
         }
 
+        if let Some(platform) = &config.platform {
+            rustflags.push("--cfg".into());
+            rustflags.push(format!("platform=\"{}\"", platform));
+        }
+
         if let Some(devices) = &config.dev {
+            // get all [config] 'dev' settings
             for dev in devices {
                 rustflags.push("--cfg".into());
+
+                // prefix the setting
                 rustflags.push(format!("dev_{dev}"));
             }
         }
 
         if let Some(ips) = &config.ip {
+            // get all [config] 'ip' settings
             for ip in ips {
                 rustflags.push("--cfg".into());
+
+                // prefix the setting
                 rustflags.push(format!("ip_{ip}"));
             }
         }
         if let Some(links) = &config.link {
+            // get all [config] 'link' settings
             for link in links {
                 rustflags.push("--cfg".into());
+
+                // prefix the setting
                 rustflags.push(format!("link_{link}"));
             }
         }
 
         if let Some(nodevs) = &config.nodev {
+            // get all [config] 'nodev' settings
             for nodev in nodevs {
                 rustflags.push("--cfg".into());
+
+                // prefix the setting
                 rustflags.push(format!("nodev_{nodev}"));
             }
         }
 
         if let Some(nouarts) = &config.nouart {
+            // get all [config] 'nodev' settings
             for nouart in nouarts {
                 rustflags.push("--cfg".into());
+
+                // prefix the setting
                 rustflags.push(format!("nouart_{nouart}"));
             }
         }
     }
 
+    // we don't need to handle the linker script for clippy
     if task != "clippy" {
         if let Some(link) = &config.link {
             let filename = link["script"].clone();
 
+            // do we have a linker script ?
             if !filename.is_empty() {
                 let mut contents = match fs::read_to_string(format!("{}/{}", wks_path, filename)) {
                     Ok(c) => c,
@@ -147,34 +208,45 @@ pub fn generate_args(
                     }
                 };
 
+                // replace the placeholders with the values from the TOML
                 if let Some(link) = &config.link {
                     for l in link.iter() {
                         match l.0.as_str() {
                             "arch" => contents = contents.replace("${ARCH}", l.1),
                             "load-address" => contents = contents.replace("${LOAD-ADDRESS}", l.1),
-                            "script" => {}
+                            "script" => {} // do nothing for the script option
                             _ => eprintln!("ignoring unknown option '{} = {}'", l.0, l.1),
                         }
                     }
                 }
 
+                // construct the path to the target directory
                 let path = format!(
                     "{}/target/{}/{}",
                     wks_path,
                     target,
                     profile.to_string().to_lowercase()
                 );
+
+                // make sure the target directory exists
                 if !std::path::Path::new(&path).exists() {
+                    // if not, create it
                     let _ = create_dir_all(&path);
                 }
 
+                // everything is setup, now create the linker script
+                // in the target directory
                 let mut file = File::create(format!("{}/kernel.ld", path)).unwrap();
                 let _ = file.write_all(contents.as_bytes());
 
+                // pass the script path to the rustflags
                 rustflags.push(format!("-Clink-args=-T{}/kernel.ld", path));
             }
         }
     }
+
+    // pass the collected rustflags
+    // !! this overwrites the build.rustflags from the target Cargo.toml !!
     if !rustflags.is_empty() {
         let flat = rustflags.join(" ");
         cmd.arg("--config");
