e73efef3b8e8a2d5afdfa3f3b3e277dc79a7245e
Stefan Hertenberger <erde74@gmail.com>
2023-04-20 10:08:52 +0200

    cleanup and documentation
    Signed-off-by: Stefan Hertenberger <erde74@gmail.com>

--- riscv64/src/platform/mod.rs
+++ riscv64/src/platform/mod.rs
@@ -3,7 +3,7 @@ pub mod nezha;
 #[cfg(platform = "nezha")]
 pub use crate::platform::nezha::*;
 
-#[cfg(not(platform = "virt"))]
+#[cfg(not(platform = "nezha"))]
 pub mod virt;
-#[cfg(not(platform = "virt"))]
+#[cfg(not(platform = "nezha"))]
 pub use crate::platform::virt::*;
