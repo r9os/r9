61f5df3be8dd404c977597195010a932b9aadfca
Daniel Maslowski <info@orangecms.org>
2023-04-29 19:07:29 +0200

    riscv: document PTE
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- riscv64/src/memory.rs
+++ riscv64/src/memory.rs
@@ -1,6 +1,8 @@
 use crate::platform::PHYSICAL_MEMORY_OFFSET;
 
 /// Convert physical address to virtual address
+/// See 4.3.2 Virtual Address Translation Process,
+/// Volume II: RISC-V Privileged Architectures V20211203 p82
 #[inline]
 pub const fn phys_to_virt(paddr: usize) -> usize {
     PHYSICAL_MEMORY_OFFSET + paddr
