b3354229c9e4d3f8f615ccc750aee4317def3b8d
Daniel Maslowski <info@orangecms.org>
2023-05-01 19:17:48 +0200

    riscv: factor out walk_dt, print binary sections
    This aligns the riscv64 platform/virt kernel linker script with the aarch64 one.
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- riscv64/src/memory.rs
+++ riscv64/src/memory.rs
@@ -3,6 +3,15 @@ use crate::platform::PHYSICAL_MEMORY_OFFSET;
 /// Convert physical address to virtual address
 /// See 4.3.2 Virtual Address Translation Process,
 /// Volume II: RISC-V Privileged Architectures V20211203 p82
+/// va.off = pa.off
+///
+/// Physical address:
+///         | VPN[2] | VPN[1] | VPN[0] | offset |
+///         |[38..30]|[29..21]|[20..12]|[11..0] |
+/// Virtual address:
+/// |     PPN[2]     | PPN[1] | PPN[0] | offset |
+/// |    [55..30]    |[29..21]|[20..12]|[11..0] |
+/// NOTE: PPN[2] is 26 bits wide, VPN[2] only 9
 #[inline]
 pub const fn phys_to_virt(paddr: usize) -> usize {
     PHYSICAL_MEMORY_OFFSET + paddr
