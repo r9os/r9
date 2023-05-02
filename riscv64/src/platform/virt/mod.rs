168425441e1e9ff2504461516a0d5f6e0fbc45fb
Daniel Maslowski <info@orangecms.org>
2023-05-02 08:15:58 +0200

    riscv/platform/virt: fix up memory address offset
    This causes hanging, but would be correct.
    See also rCore `kernel/src/arch/riscv/consts.rs`.
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- riscv64/src/platform/virt/mod.rs
+++ riscv64/src/platform/virt/mod.rs
@@ -1,7 +1,7 @@
 pub mod devcons;
 use port::println;
 
-pub const PHYSICAL_MEMORY_OFFSET: usize = 0xFFFF_FFFF_C000_0000;
+pub const PHYSICAL_MEMORY_OFFSET: usize = 0xFFFF_FFFF_4000_0000;
 
 pub fn platform_init() {
     println!("platform_init");
