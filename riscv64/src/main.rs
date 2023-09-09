79a726529c2aaa00f4549808bb3aed8c30454129
Marcus Weiner <marcus.weiner@gmail.com>
2023-09-10 00:14:59 +0200

    Implement page table reading and writing

--- riscv64/src/main.rs
+++ riscv64/src/main.rs
@@ -13,14 +13,15 @@ mod runtime;
 mod sbi;
 mod uart16550;
 
+use bit_field::BitField;
+use bitflags::{bitflags, Flags};
 use port::{print, println};
 
 use crate::{
     memory::phys_to_virt,
     platform::{devcons, platform_init},
 };
-use core::ffi::c_void;
-use core::slice;
+use core::{ffi::c_void, ptr::read_volatile, ptr::write_volatile, slice};
 use port::fdt::DeviceTree;
 
 #[cfg(not(test))]
@@ -175,6 +176,96 @@ fn where_are_we() {
     println!("YOU ARE HERE (approx.): {p:#x?}");
 }
 
+bitflags! {
+    #[derive(Debug)]
+    pub struct PageTableFlags: u8 {
+        const D = 1 << 7;
+        const A = 1 << 6;
+        const G = 1 << 5;
+        const U = 1 << 4;
+        const X = 1 << 3;
+        const W = 1 << 2;
+        const R = 1 << 1;
+        const V = 1;
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct SizedInteger<const N: usize>(u64);
+
+#[derive(Debug)]
+struct NumberTooLarge;
+
+impl<const N: usize> TryFrom<u64> for SizedInteger<N> {
+    type Error = NumberTooLarge;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        if (value.leading_zeros() as usize) < 64 - N {
+            return Err(NumberTooLarge);
+        }
+        Ok(Self(value))
+    }
+}
+
+impl<const N: usize> From<SizedInteger<N>> for u64 {
+    fn from(value: SizedInteger<N>) -> Self {
+        value.0
+    }
+}
+
+#[derive(Debug)]
+struct PageTableEntry {
+    ppn2: SizedInteger<26>,
+    ppn1: SizedInteger<9>,
+    ppn0: SizedInteger<9>,
+    flags: PageTableFlags,
+}
+
+impl PageTableEntry {
+    pub fn serialize(&self) -> u64 {
+        let mut out = 0u64;
+        out.set_bits(0..=7, self.flags.bits() as _);
+        out.set_bits(10..=18, self.ppn0.into());
+        out.set_bits(19..=27, self.ppn1.into());
+        out.set_bits(28..=53, self.ppn2.into());
+        out
+    }
+
+    pub unsafe fn write_to(&self, addr: u64) {
+        unsafe { write_volatile(addr as *mut u64, self.serialize()) }
+    }
+}
+
+impl From<u64> for PageTableEntry {
+    fn from(value: u64) -> Self {
+        let flags = PageTableFlags::from_bits(value.get_bits(0..=7) as _).unwrap();
+        Self {
+            ppn2: value.get_bits(28..=53).try_into().unwrap(),
+            ppn1: value.get_bits(19..=27).try_into().unwrap(),
+            ppn0: value.get_bits(10..=18).try_into().unwrap(),
+            flags,
+        }
+    }
+}
+
+struct PageTable {
+    addr: u64,
+}
+
+impl PageTable {
+    const ENTRY_SIZE: u64 = 8;
+
+    pub fn new(addr: u64) -> Self {
+        Self { addr }
+    }
+
+    pub fn dump_entry(&self, at: u16) -> PageTableEntry {
+        let addr = self.addr + (at as u64 * Self::ENTRY_SIZE);
+        let val = unsafe { read_volatile(addr as *const u64) };
+        val.into()
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn main9(hartid: usize, dtb_ptr: u64) -> ! {
     // devcons::init_sbi();
@@ -201,8 +292,51 @@ pub extern "C" fn main9(hartid: usize, dtb_ptr: u64) -> ! {
         where_are_we();
     }
 
+    extern "C" {
+        static boot_page_table: *const c_void;
+    }
+
+    let bpt_addr = unsafe { (&boot_page_table) as *const _ as u64 };
+    println!("table addr: {:#x}", bpt_addr);
+    let bpt = PageTable::new(bpt_addr);
+    println!("{:?}", bpt.dump_entry(508));
+    println!("{:?}", bpt.dump_entry(509));
+    println!("{:?}", bpt.dump_entry(510));
+    println!("{:?}", bpt.dump_entry(511));
+
     #[cfg(not(test))]
     sbi::shutdown();
     #[cfg(test)]
     loop {}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{PageTableEntry, PageTableFlags};
+
+    #[test]
+    fn test_pagetableentry() {
+        {
+            let entry = PageTableEntry {
+                ppn2: 0.try_into().unwrap(),
+                ppn1: 1.try_into().unwrap(),
+                ppn0: 2.try_into().unwrap(),
+                flags: PageTableFlags::W.union(PageTableFlags::R),
+            };
+            assert_eq!(entry.serialize(), 0b1_000000010_00_00000110);
+        }
+
+        {
+            let entry = PageTableEntry {
+                ppn2: 0x03f0_0000.try_into().unwrap(),
+                ppn1: 1.try_into().unwrap(),
+                ppn0: 2.try_into().unwrap(),
+                flags: PageTableFlags::W.union(PageTableFlags::R),
+            };
+            assert_eq!(
+                entry.serialize(),
+                0b11_1111_0000_0000_0000_0000_0000__000000001__000000010__00__00000110
+            );
+        }
+    }
 }
