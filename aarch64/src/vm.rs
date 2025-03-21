c4999cdea62f2ded2bc608db90fe2ec1abe5de62
Graham MacDonald <grahamamacdonald@gmail.com>
2025-03-21 17:53:04 +0000

    vm: remove pre-existing entry check and some unused code
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- aarch64/src/vm.rs
+++ aarch64/src/vm.rs
@@ -283,7 +283,6 @@ fn recursive_table_addr(pgtype: RootPageTableType, va: usize, level: Level) -> u
 pub enum PageTableError {
     AllocationFailed(PageAllocError),
     EntryIsNotTable,
-    EntryAlreadyInUse,
     PhysRangeIsZero,
     PhysRangeIsNotOnPageBoundary,
 }
@@ -430,11 +429,6 @@ impl RootPageTable {
                 return Err(err);
             }
         };
-
-        if dest_entry.valid() {
-            println!("error:vm:map_to:entry already in use. va:{:#x}", va);
-            return Err(PageTableError::EntryAlreadyInUse);
-        }
 
         // Entries at level 3 should have the page flag set
         let entry =
